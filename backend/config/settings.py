af22d143cc5d154fc9f8c681f756ca4ee75c54d5
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2022-12-23 22:29:59 +1300

    wip

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -3,6 +3,7 @@
 import os
 from datetime import timedelta
 from pathlib import Path
+from urllib.parse import urlparse
 
 from django.core.management.utils import get_random_secret_key
 
@@ -142,7 +143,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -151,6 +151,23 @@ DATABASES = {
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": 5432,
+    }
+}
+# online - for prod
+if os.getenv("DATABASE_URL", "") != "":
+    r = urlparse(os.environ.get("DATABASE_URL"))
+    DATABASES["default"]["NAME"] = os.path.relpath(str(r.path), "/")
+    DATABASES["default"]["USER"] = str(r.username)
+    DATABASES["default"]["PASSWORD"] = str(r.password)
+    DATABASES["default"]["HOST"] = str(r.hostname)
+    DATABASES["default"]["PORT"] = int(r.port)
+
+
+# Cached Database - Redis
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": os.getenv("REDIS_URL", "redis://cache:6379"),
     }
 }
 
