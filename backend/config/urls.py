e8eada6a1eb9da2002b6d9e9ba6019ac595fb5dd
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2022-12-24 17:00:53 +1300

    wip

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,12 @@
 """Main urls."""
 
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import include, path
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("", lambda request: render(request, "index.html")),
     path("admin/", admin.site.urls),
     path("api/v1/", include("project.urls")),
     path("api/", RedirectView.as_view(url="api/v1", permanent=True), name="api_index"),
