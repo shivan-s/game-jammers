95e8477905c471c8e4729485ead473f764f2faec
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-02-27 09:45:56 +1300

    wip

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -1,4 +1,4 @@
-/r/ @ts-check
+// @ts-check
 /**
  * This file is included in `/next.config.mjs` which ensures the app isn't built with invalid env vars.
  * It has to be a `.mjs`-file to be imported there.
@@ -11,7 +11,7 @@ const _serverEnv = serverSchema.safeParse(process.env);
 if (!_serverEnv.success) {
   console.error(
     "❌ Invalid environment variables:\n",
-    ...formatErrors(_serverEnv.error.format())
+    ...formatErrors(_serverEnv.error.format()),
   );
   throw new Error("Invalid environment variables");
 }
